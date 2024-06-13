8bc0ac2560a6691b29e3d5956d5c6a037dcf71df
Sanjoy-droid <sanjoy@sanjoydroid.com>
2024-06-13 23:59:10 +0530

    converting jsx component to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./App.css";
 import { Person } from "./Components/Person";
 import { UserProvider } from "./Components/UserContextProvider";
+import UserProfileEditor from "./Components/UserProfileEditor";
 
 function App() {
   // let name: string = "sanjoy";
@@ -9,19 +11,36 @@ function App() {
   // let isMarried: boolean = true;
   // let ages: number[] = [45, 66];
 
-  const fetchUserData = () => ({ name: "Tom", age: 45, isMarried: true });
+  // const fetchUserData = () => ({ name: "Tom", age: 45, isMarried: true });
 
-  const userData = fetchUserData();
-  return (
-    <UserProvider>
-      <Person name={"Sanjoy"} age={22} isMarried={false} />
+  // const userData = fetchUserData();
+
+  const [user, setUser] = useState({ name: "sanjoy", email: "s@t.com" });
+
+  const handleUpdate = (updatedUser: { name: string; email: string }) => {
+    console.log("Updated User:", updatedUser);
+    setUser(updatedUser);
+    return (
+      <UserProvider>
+        {/* <Person name={"Sanjoy"} age={22} isMarried={false} />
       <Person
         name={userData.name}
         age={userData.age}
         isMarried={userData.isMarried}
-      />
-    </UserProvider>
-  );
+      /> */}
+
+        <div>
+          <h1>User Profile</h1>
+          <UserProfileEditor user={user} onUpdate={handleUpdate} />
+          <div>
+            <h2>Current User Info</h2>
+            <p>Name: {user.name}</p>
+            <p>Email: {user.email}</p>
+          </div>
+        </div>
+      </UserProvider>
+    );
+  };
 }
 
 export default App;
